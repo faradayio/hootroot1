dd9af43df817b573e163881ffdaf9aa32d2db1b3
Derek Kastner <dkastner@gmail.com>
2011-02-16 16:56:01 -0500

    Added RouteView specs

--- spec/javascripts/views/RouteViewSpec.js
+++ spec/javascripts/views/RouteViewSpec.js
@@ -1,7 +1,27 @@
 describe('RouteView', function() {
   var routeView
+  var directions
 
   beforeEach(function() {
     routeView = new RouteView()
+    directions = new Directions('Lansing, MI', 'Ann Arbor, MI')
+    directions.directionResult = result
+  })
+
+  describe('#update', function() {
+    it('updates the #route div with directions', function() {
+      setFixtures('<div id="route"></div>')
+      routeView.update(directions)
+      expect($('#route').html()).toContain('Total emissions')
+    })
+  })
+
+  describe('#updateSegmentEmissions', function() {
+    it('updates the emissions of a segment', function() {
+      setFixtures('<div id="route"></div>')
+      routeView.update(directions)
+      routeView.updateSegmentEmissions(0, 'BINGO')
+      expect($('#segment_0').html()).toContain('BINGO')
+    })
   })
 })
