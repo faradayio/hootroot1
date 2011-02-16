21d6d60c8ca267d16d46aa3b26ae0fe079791c8f
Derek Kastner <dkastner@gmail.com>
2011-02-16 16:55:25 -0500

    Globalized google and google directions results

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -6,4 +6,45 @@ beforeEach(function() {
           && player.isPlaying;
     }
   })
+  google = {
+    maps: {
+      Map: function() {},
+      LatLng: function() {},
+      MapTypeId: { ROADMAP: 'roadmap' },
+      DirectionsService: function() { this.route = function() {} },
+      DirectionsTravelMode: { DRIVING: 'DRIVING' },
+      DirectionsStatus: { OK: 'OK' },
+      DirectionsRenderer: function() {} } }
+
+  result = {
+    routes: [{
+      summary: 'I-96E',
+      legs: [{
+        distance: {
+          text: '66.3 mi',
+          value: 106738
+        },
+        duration: {
+          text: '1 hour 9mins',
+          value: 4158
+        },
+        end_address: 'Ann Arbor, MI, USA',
+        start_address: 'Lansing, MI, USA',
+        steps: [
+          { travel_mode: 'DRIVING',
+            distance: { value: 688 },
+            instructions: 'Go there' },
+          { travel_mode: 'DRIVING',
+            distance: { value: 128 },
+            instructions: 'Go there' },
+          { travel_mode: 'DRIVING',
+            distance: { value: 45 },
+            instructions: 'Go there' },
+          { travel_mode: 'DRIVING',
+            distance: { value: 9025 },
+            instructions: 'Go there' },
+        ]
+      }]
+    }]
+  }
 });
--- spec/javascripts/models/DirectionsSpec.js
+++ spec/javascripts/models/DirectionsSpec.js
@@ -1,50 +1,7 @@
 describe('Directions', function() {
   var directions
-  var result
 
   beforeEach(function() {
-    google = {
-      maps: {
-        Map: function() {},
-        LatLng: function() {},
-        MapTypeId: { ROADMAP: 'roadmap' },
-        DirectionsService: function() { this.route = function() {} },
-        DirectionsTravelMode: { DRIVING: 'DRIVING' },
-        DirectionsStatus: { OK: 'OK' },
-        DirectionsRenderer: function() {} } }
-
-      result = {
-        routes: [{
-          summary: 'I-96E',
-          legs: [{
-            distance: {
-              text: '66.3 mi',
-              value: 106738
-            },
-            duration: {
-              text: '1 hour 9mins',
-              value: 4158
-            },
-            end_address: 'Ann Arbor, MI, USA',
-            start_address: 'Lansing, MI, USA',
-            steps: [
-              { travel_mode: 'DRIVING',
-                distance: { value: 688 },
-                instructions: 'Go there' },
-              { travel_mode: 'DRIVING',
-                distance: { value: 128 },
-                instructions: 'Go there' },
-              { travel_mode: 'DRIVING',
-                distance: { value: 45 },
-                instructions: 'Go there' },
-              { travel_mode: 'DRIVING',
-                distance: { value: 9025 },
-                instructions: 'Go there' },
-            ]
-          }]
-        }]
-      }
-
     directions = new Directions('Lansing, MI', 'Ann Arbor, MI')
   })
 
