c35b511b5592b7605e6553facc93a4294d29ade0
Derek Kastner <dkastner@gmail.com>
2012-08-29 10:04:13 -0500

    Use env for settings

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,5 +1,4 @@
-host = ENV['STATSD_HOST']
-host ||= (Rails.env.production? ? 'stats.brighterplanet.com' : '127.0.0.1')
+host = ENV['STATSD_HOST'] || '127.0.0.1'
 port = ENV['STATSD_PORT'] || '8125'
 Stats = Statsd.new(host, port)
 Stats.namespace = 'hootroot'
